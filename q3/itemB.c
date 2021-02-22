c8282a43ee6280a1b881ad58b7e79700622e4b1f
Mateus Paiva <72986265+MatteusP@users.noreply.github.com>
2021-02-22 15:04:13 -0300

    Update itemB.c

--- q3/itemB.c
+++ q3/itemB.c
@@ -13,41 +13,39 @@ void numeros(){
 	pid = fork();
 	for (i = 0; i < (n/2)+2; ++i){
 		if (pid < 0){
-      perror("fork");
-      exit(1);
-    }
-    if (pid == 0){
-      //O código aqui dentro será executado no processo filho
-      printf("Filho: ");
-      printf("%d \n", v[i+1]);
-      i++;
+      			perror("fork");
+     			exit(1);
+    		
+		if (pid == 0){
+      			//O código aqui dentro será executado no processo filho
+      			printf("Filho: ");
+      			printf("%d \n", v[i+1]);
+      			i++;
 			
-    }else{
-      //O código neste trecho será executado no processo pai
-      printf("Pai: ");
-      printf("%d \n", v[i]);
-    }		
+    		}else{
+      			//O código neste trecho será executado no processo pai
+      			printf("Pai: ");
+      			printf("%d \n", v[i]);
+    		}		
 	 }
-	
 	 exit(0);
 }
 
 int main() {
+	printf("Entre com o tamanho do vetor: ");
+    	scanf("%d", &n);
+    	v = (int *)malloc(n * sizeof(int));
 
-    printf("Entre com o tamanho do vetor: ");
-    scanf("%d", &n);
-    v = (int *)malloc(n * sizeof(int));
+    	for (i = 0; i < n; ++i) {
+        	scanf("%d", &v[i]);
+    	}
 
-    for (i = 0; i < n; ++i) {
-        scanf("%d", &v[i]);
-    }
+    	/*for (i = 0; i < n; ++i) {
+        	printf("%d ", v[i]);
+    	}*/
+    	numeros();
 
-    /*for (i = 0; i < n; ++i) {
-        printf("%d ", v[i]);
-    }*/
-    numeros();
-
-    printf("\n");
-    free(v);
-    return 0;
+    	printf("\n");
+    	free(v);
+    	return 0;
 }
