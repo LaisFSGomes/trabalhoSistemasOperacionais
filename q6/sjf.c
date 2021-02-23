f2c61e4fc8c9312766d96853f092e4a1d337192a
laisfsgomes <laisfsgomes@gmail.com>
2021-02-23 16:31:51 -0300

    algoritmo sjf implementado

---
+++ q6/sjf.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+typedef struct{
+	int id;
+	int tempo;
+} processo;
+
+//função que ordena de acordo com o tempo de execução de cada processo. O algoritmo é o insertion sort
+void ordena(processo *v, int N){
+	int i, j;
+	processo aux;
+	for (i = 1; i < N; i++){
+		aux = v[i];
+		for (j=i; (j>0) && (aux.tempo < v[j-1].tempo); j--)
+			v[j] = v[j-1];
+		v[j] = aux;
+	}
+}
+
+int main(){
+	int qtd; //quantidade de processos na fila de execução
+	printf("quantos processos estao na fila: ");
+	scanf("%d", &qtd);
+	
+	processo filap[qtd]; //fila de processos
+	
+	//input dos processos
+	printf("diga, na ordem de chegada, as informacoes de cada processo:\n");
+	int i, j;
+	for (i=0; i<qtd; i++){
+		printf("\n------------------PROCESSO %d------------------\n", (i+1));
+		printf("tempo de execucao (em segundos): ");
+		scanf("%d", &filap[i].tempo);
+		filap[i].id = i+1;
+	}
+	
+	//ordenando de acordo com o tempo de execução
+	ordena(filap, qtd);
+
+	//processos
+	system("cls");
+	for (i=0; i<qtd; i++){
+		printf("\n------------------PROCESSO %d------------------\n", (i+1));
+		printf("id = %d \nTempo de execucao em segundos: %d\n", filap[i].id, filap[i].tempo);
+		Sleep(filap[i].tempo*1000);
+	}
+	
+}
