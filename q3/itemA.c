eb46857cc2325bfbce7430538d659ed01dd17af7
Mateus Paiva <72986265+MatteusP@users.noreply.github.com>
2021-02-25 22:50:38 -0300

    Update itemA.c

--- q3/itemA.c
+++ q3/itemA.c
@@ -5,14 +5,17 @@
 
 int main(void)
 {
-    int i;
-    pid_t pid;
- 
-
+    int i; 
+    pid_t pid; // identificador de processos, cada processo tem um valor diferente de pid
+    
+    //dentro do processo filho, o pid tem valor 0
+    //dentro do processo pai, o pid tem o valor do processo filho
+    //a fork() retorna um valor negativo, caso tenha ocorrido algum erro.
     if((pid = fork()) < 0){
         perror("fork");
         exit(1);
     }
+    //caso pid = 0 el cria o processo filho
     if (pid == 0){
         //O código aqui dentro será executado no processo filho
         printf("pid do Filho: %d\n", getpid());
@@ -20,9 +23,10 @@ int main(void)
     }else{
         //O código neste trecho será executado no processo pai
         printf("pid do Pai: %d\n", getpid());
-   
     }
-
+    //    F0       Haverá um processo filho criado pelo primeiro fork() There will be 1 child process created by first fork
+    //  /     \
+    //F1      F1   Haverão 2 processos filhos criados pelo segundo fork()
     fork();
     if(pid == 0){
       printf("pid do Filho 1: %d\n", getpid());
